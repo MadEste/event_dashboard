ad42327765d9fec1b8566530b64f749203cdf8e7
Este <estevansyfried@gmail.com>
2015-05-08 22:19:12 -0700

    Links destroy on parent events deletion

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
 	belongs_to :user
-	has_many :links
+	has_many :links, dependent: :destroy
 
 	accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
 
