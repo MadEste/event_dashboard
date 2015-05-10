bd249f163f05611dcfaba7b4b68319c63d212c35
Este <estevansyfried@gmail.com>
2015-05-09 18:44:58 -0700

    dynamic routing if user logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
 
-
-
 	devise_for :users
 	resources :events
-
-	root "events#index"
 	
 	get '/home', to: 'page#home'
 	get '/help', to: 'page#help'
 	get '/about', to: 'page#about'
+
+	authenticated :user do
+		root 'events#index', as: "authenticated_root"
+	end
+
+	root "page#home"
 
 end
