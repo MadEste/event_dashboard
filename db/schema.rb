b85316bb960c4c6d591296733d5dd4ced66d683c
Este <estevansyfried@gmail.com>
2015-05-23 12:47:20 -0700

    changed database to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150509000624) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -29,7 +32,7 @@ ActiveRecord::Schema.define(version: 20150509000624) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "links", ["event_id"], name: "index_links_on_event_id"
+  add_index "links", ["event_id"], name: "index_links_on_event_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -46,7 +49,8 @@ ActiveRecord::Schema.define(version: 20150509000624) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "links", "events"
 end
