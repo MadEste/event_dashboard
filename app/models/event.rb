d00e69dc81e852fbdfb66a4a3f581a487d307ecf
Este <estevansyfried@gmail.com>
2015-06-12 15:20:40 -0700

    Created Asset Model and Relationship

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,10 +3,12 @@ class Event < ActiveRecord::Base
 	belongs_to :user
 	has_many :links, dependent: :destroy
 	has_one :schedule, dependent: :destroy
+	has_many :assets, dependent: :destroy
 	#has_many :days, through: :schedule
 
 	accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
 	accepts_nested_attributes_for :schedule, allow_destroy: true
+	accepts_nested_attributes_for :assets, allow_destroy: true
 	#accepts_nested_attributes_for :days, allow_destroy: true
 
 	validates :name, :description, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607005817) do
+ActiveRecord::Schema.define(version: 20150612221544) do
+
+  create_table "assets", force: :cascade do |t|
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "assets", ["event_id"], name: "index_assets_on_event_id"
 
   create_table "dayevents", force: :cascade do |t|
     t.integer  "day_id"
