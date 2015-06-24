63ec896a166b5928409433358002e8f3f23b7406
Este <estevansyfried@gmail.com>
2015-06-23 21:31:58 -0700

    styling show Page Schedule

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -5,7 +5,8 @@ class Asset < ActiveRecord::Base
 		:path => "assets/:id/:basename.:extension",
 		:storage => :s3,
 		:s3_credentials => "#{Rails.root}/config/amazon_s3.yml",
-		:bucket => "eventdash"
+		:bucket => "eventdash",
+		:url => ":s3_domain_url"
 	do_not_validate_attachment_file_type :uploaded_file
 	validates_attachment_size :uploaded_file, :less_than => 10.megabytes
 	validates_attachment_presence :uploaded_file
